fe32a983146d54d02d64fa578829864fb58ae074
mikk39k1 <mikk39k1@stud.kea.dk>
2023-04-17 21:57:36 +0200

    added home dir

--- src/main/java/com/example/ourwishlist/controller/HomepageController.java
+++ src/main/java/com/example/ourwishlist/controller/HomepageController.java
@@ -6,13 +6,14 @@ import com.example.ourwishlist.service.FriendsListService;
 import com.example.ourwishlist.service.WishListService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@org.springframework.stereotype.Controller
+@Controller
 public class HomepageController {
     @Autowired
     WishListService wishListService;
